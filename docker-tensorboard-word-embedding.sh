2dfe16b13732eb1af66a2f5d1eb3bf30efbba980
ductri <ductricse@gmail.com>
2018-06-29 21:28:24 +0700

    ok

---
+++ docker-tensorboard-word-embedding.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+nvidia-docker run --name trind_word_embedding_visualization --rm \
+-v `pwd`:/source/ \
+-p 0.0.0.0:2609:2609 \
+tensorflow/tensorflow:nightly-gpu-py3 \
+/bin/bash -c "tensorboard --logdir=/source/checkpoint/%1 --port=2609"
