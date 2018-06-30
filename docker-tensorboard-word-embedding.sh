1fab40b115179f9e67fcbce650fdc4e65bcb9f1e
ductri <ductricse@gmail.com>
2018-07-01 04:03:45 +0700

    temp

--- docker-tensorboard-word-embedding.sh
+++ docker-tensorboard-word-embedding.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nvidia-docker run --name trind_word_embedding_visualization --rm \
+nvidia-docker run --name trind_word_embedding_visualization -d --rm \
 -v `pwd`:/source/ \
 -p 0.0.0.0:2609:2609 \
 tensorflow/tensorflow:nightly-gpu-py3 \
